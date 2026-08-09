f04404e44a5a1f9b4f77df6441438d615b141443
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-08-09 15:48:55 +0530

    Time: 0 ms (100%), Space: 44 MB (88.45%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+
+        int low = 1;
+        int high = n-2;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            if((nums[mid]>nums[mid-1]) && (nums[mid]>nums[mid+1])) return mid;
+            //it means the current element is on the increasing curve and our peak element lies beyond it. hence we need to look after mid.
+            else if(nums[mid]>nums[mid-1]) low=mid+1;
+            //it means the current element is on the decreasing curve and our peak element lies before it. hence we need to look before mid.
+            else if(nums[mid]>nums[mid+1]) high=mid-1;
+            //if both are not the case go on either side i.e. either left or right.
+            else low = mid+1;
+        }
+
+        return -1;
+        
+    }
+}
