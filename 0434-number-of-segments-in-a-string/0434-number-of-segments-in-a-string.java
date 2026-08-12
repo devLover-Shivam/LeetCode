3b6ec22dc6767f08ee98286339d529cdb096f6f7
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-08-12 19:32:43 +0530

    Time: 0 ms (100%), Space: 42.7 MB (31.38%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
 
         int segmentCount = 0;
         for(int i=0;i<s.length();i++){
-            //kya 
+            //kya current character ek non-space character hai AND kya ye word ka start hai?
             if(s.charAt(i)!=' ' && (i == 0 || s.charAt(i-1)== ' ')){
                 segmentCount ++;
             }
