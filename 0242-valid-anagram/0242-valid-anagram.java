a12d1708a2a26474b241f0fed78cfa954f6a0c28
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-08-12 14:46:59 +0530

    Time: 5 ms (61.73%), Space: 44.1 MB (90.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,14 +4,14 @@ class Solution {
         //anagrams must have equal length
         if (s.length() != t.length()) return false;
 
-        // for string s increasing the frequency
+        // for valid anagrams the length of both strings will always be same so we dont need two for loops
         for(int i=0;i<s.length();i++){
+            //for string s increasing the frequency
             freq[s.charAt(i) - 'a'] ++;
-        }
-        //for string t decreasing the frequency
-        for(int i =0;i<t.length();i++){
+            //for string t decreasing the frequency
             freq[t.charAt(i)-'a'] --;
         }
+        
         //now if any of the freq elements is not equal to 0 return false
         for(int i = 0;i<26;i++){
             if(freq[i] != 0) return false;
