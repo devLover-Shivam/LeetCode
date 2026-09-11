630229e8c1d8c9d02c1951f5bb07d79d63160b9b
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-09-11 15:06:49 +0530

    Time: 1 ms (6.69%), Space: 43 MB (70.02%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+/* 
+RECURSIVE APPROACH
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -31,4 +33,36 @@ class Solution {
         preorder(node.left, result);
         preorder(node.right, result);
     }
-}
+} 
+*/
+
+//ITERATIVE APPROACH USING STACK
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preorder = new ArrayList<>();
+        if(root == null) return preorder;
+        Stack<TreeNode> st = new Stack <TreeNode>();
+
+        st.push(root);
+
+        while(!st.isEmpty()){
+            root =  st.pop();
+            preorder.add(root.val);
+
+            if(root.right!=null){
+                st.push(root.right);
+            }
+
+            if(root.left != null){
+                st.push(root.left);
+            }
+
+            
+        }
+
+        return preorder;
+
+    }
+
+} 
